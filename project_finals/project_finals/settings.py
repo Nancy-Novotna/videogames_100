400eed7f182d8a48f4c115c85517f142b12be7b6
Nancy Novotna <d23974@oa-opava.cz>
2026-05-14 10:16:41 +0200

    allowed hosts

--- project_finals/project_finals/settings.py
+++ project_finals/project_finals/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vbw06z%3*1pe!()v0*3f)+ndmh4p-)-0-2x=dt*gp4&f7dp@#7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nanca.pythonanywhere.com']
 
 
 # Application definition
