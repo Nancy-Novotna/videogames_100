152c9c8bc49d4271cade97fded8ec256d7d7817f
Nancy Novotna <d23974@oa-opava.cz>
2026-06-11 10:23:42 +0200

    pridani django slug

--- project_finals/videogames_100/urls.py
+++ project_finals/videogames_100/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("games/", views.games, name="games"),
+    path("<slug:slug>/", views.videogame, name="videogame"),
 ]
